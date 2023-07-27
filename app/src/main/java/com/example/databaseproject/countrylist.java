dd0d02442f316b3cea9b961291a92497dc14fe23
tanjilalom <tanjil15-3079@diu.edu.bd>
2023-07-27 21:39:37 +0600

    First Commit

--- app/src/main/java/com/example/databaseproject/countrylist.java
+++ app/src/main/java/com/example/databaseproject/countrylist.java
@@ -1,17 +1,19 @@
 package com.example.databaseproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 public class countrylist extends AppCompatActivity {
 
     ListView mylistview;
+
+    SearchView sv;
     String [] countryname;
     String selectedvalue;
 
@@ -22,15 +24,30 @@ public class countrylist extends AppCompatActivity {
 
         mylistview = findViewById(R.id.listviewid);
         countryname = getResources().getStringArray(R.array.countryname);
+        sv = findViewById(R.id.searchviewid);
 
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, R.layout.sampleview, R.id.textviewsample, countryname);
-
         mylistview.setAdapter(myAdapter);
+
+
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                myAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
 
         mylistview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
 
                 selectedvalue = countryname[position];
 
@@ -68,8 +85,6 @@ public class countrylist extends AppCompatActivity {
                     myintent.putExtra("name", "japan");
                     startActivity(myintent);
                 }
-
-
 
             }
         });
